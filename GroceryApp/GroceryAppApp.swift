b5324c104ab86539aa1db5b4ee6575f72d012417
MehdiALOMER <mehdi.alomer98@gmail.com>
2024-08-30 22:08:21 +0300

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Mehdi on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
